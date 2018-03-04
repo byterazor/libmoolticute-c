680c1386aa5a36d6ad1486fbfe18109dfb0b2850
Dominik Meyer <dmeyer@federationhq.de>
2018-03-04 14:50:35 +0100

    ADD: only add callback if it not already exist

--- src/moolticute_register_cb.c
+++ src/moolticute_register_cb.c
@@ -3,7 +3,24 @@
 
 void moolticute_register_cb(const char *cmd, void (*cb)(struct json_object *jObj))
 {
-  strncpy(mContext.callbacks[mContext.cb_nr].cmd, cmd, strlen(cmd));
-  mContext.callbacks[mContext.cb_nr].cb=cb;
-  mContext.cb_nr++;
+  int i=0;
+  int found=0;
+  /*
+  * first check if a callback is already registered
+  */
+  for(i=0; i<mContext.cb_nr; i++)
+  {
+    if (strcmp(mContext.callbacks[mContext.cb_nr].cmd, cmd) == 0)
+    {
+      found=1;
+      break;
+    }
+  }
+
+  if (found == 0)
+  {
+    strncpy(mContext.callbacks[mContext.cb_nr].cmd, cmd, strlen(cmd));
+    mContext.callbacks[mContext.cb_nr].cb=cb;
+    mContext.cb_nr++;
+  }
 }
